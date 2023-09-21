d2361e164066628735f80843cc71a4c3610a23d6
nuyh <hjaehyun25@gmail.com>
2023-09-21 22:12:40 +0900

    build: init

---
+++ src/main/java/com/pknu/studypro/StudyproApplication.java
@@ -0,0 +1,13 @@
+package com.pknu.studypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyproApplication.class, args);
+    }
+
+}
