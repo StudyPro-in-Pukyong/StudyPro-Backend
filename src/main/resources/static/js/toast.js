db0e44c7dd78ceb821308b3eb4208684b0ab574c
kung036 <sinariari3@gmail.com>
2024-10-30 19:17:46 +0900

    fix : 토스트가 더 잘보이도록 토스트 색상 변경

--- src/main/resources/static/js/toast.js
+++ src/main/resources/static/js/toast.js
@@ -1,10 +1,11 @@
-function showToast(message, duration = 3000) {
+function showToast(message, duration = 3000, isSuccess = true) {
     const toastContainer = document.getElementById('toast-container');
 
     // 토스트 알림 요소 생성
     const toast = document.createElement('div');
     toast.textContent = message;
-    toast.style.backgroundColor = '#333';
+    toast.style.backgroundColor = isSuccess ? '#28a745' : '#dc3545'; // 성공: 초록색, 실패: 빨간색
+    // toast.style.backgroundColor = '#333'; // 검정색
     toast.style.color = '#fff';
     toast.style.padding = '15px 30px';
     toast.style.borderRadius = '8px';
@@ -12,7 +13,7 @@ function showToast(message, duration = 3000) {
     toast.style.boxShadow = '0px 4px 8px rgba(0, 0, 0, 0.2)';
     toast.style.opacity = '1';
     toast.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
-    toast.style.textAlign = 'center'; // 텍스트 중앙 정렬
+    toast.style.textAlign = 'center';
 
     // 토스트 컨테이너에 추가
     toastContainer.appendChild(toast);
